88908c9eeb2a196cd48e51f1ae4734b6a2107a7b
Wentao Shang <wentaoshang@gmail.com>
2013-04-26 23:29:14 -0700

    Completely use mongodb

--- ndnfs.cc
+++ ndnfs.cc
@@ -15,9 +15,6 @@ using namespace std;
 using namespace mongo;
 using namespace boost;
 
-static const char  *file_path      = "/hello.txt";
-static const char   file_content[] = "Hello World!\n";
-static const size_t file_size      = sizeof(file_content)/sizeof(char) - 1;
 
 static DBClientConnection c;
 static const char *db_name = "ndnfs.root";
@@ -39,16 +36,17 @@ ndnfs_getattr(const char *path, struct stat *stbuf)
         return -ENOENT;
     
     BSONObj entry = cursor->next();  // There should be no two entries with the same id (absolute path)
-    
     int type = entry.getIntField("type");
     int mode = entry.getIntField("mode");
+    
     if (type == dir_type) {
         stbuf->st_mode = S_IFDIR | mode;
         stbuf->st_nlink = 1;
     } else if (type == file_type) {
         stbuf->st_mode = S_IFREG | mode;
         stbuf->st_nlink = 1;
-        stbuf->st_size = strlen(entry.getStringField("data"));  //TODO: add file size as a field in BSON document
+        int file_size = entry.getIntField("size");
+        stbuf->st_size = file_size;
     }
 
     return 0;
@@ -58,56 +56,17 @@ static int
 ndnfs_open(const char *path, struct fuse_file_info *fi)
 {
     cout << "ndnfs_open: called with path " << path << endl;
+    cout << "ndnfs_open: flag is " << fi->flags << endl;
     
     auto_ptr<DBClientCursor> cursor = c.query(db_name, QUERY("_id" << path));
     if (!cursor->more())
         return -ENOENT;
 
     //BSONObj entry = cursor->next();
-    
-    if ((fi->flags & O_ACCMODE) != O_RDONLY) //TODO: check mode
-        return -EACCES;
+    //TODO: check mode
+    //if ((fi->flags & O_ACCMODE) != O_RDONLY)
+    //    return -EACCES;
 
-    return 0;
-}
-
-static int
-ndnfs_create(const char *path, mode_t mode, struct fuse_file_info *fi)
-{
-    cout << "ndnfs_create: called with path " << path << endl;
-    
-    auto_ptr<DBClientCursor> cursor = c.query(db_name, QUERY("name" << "/"));
-    if (!cursor->more())
-        return -ENOENT;
-    
-    string abs_path(path);
-    vector< string > path_components;
-    split(path_components, abs_path, is_any_of("/")); // the first component is an empty string
-    
-    vector< BSONElement > dir_data = cursor->next()["data"].Array();
-    
-    // Build new file entry with empty content
-    BSONObj file_entry = BSONObjBuilder().append("name", path_components[1].c_str()).append("type", "file").append("data", "").obj();
-    // Append to existing BSON array  
-    c.update(db_name, BSON("name" << "/"), BSON( "$push" << BSON( "data" << file_entry ) ));
-
-    return 0;
-}
-
-static int
-ndnfs_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
-{
-    cout << "ndnfs_write: called with path " << path << endl;
-    cout << "ndnfs_write: start write at offset " << offset << " with size " << size << endl;
-    
-    auto_ptr<DBClientCursor> cursor = c.query(db_name, QUERY("name" << "/"));
-    if (!cursor->more()) {
-        string file_name(path);
-        BSONObj file_entry = BSONObjBuilder().append("name", file_name.substr(1).c_str()).append("type", "file").append("data", buf, size).obj();
-        BSONObj dir_entry = BSON( "name" << "/" << "type" << "dir" << "data" << file_entry);
-        c.insert(db_name, dir_entry);
-    }
-    
     return 0;
 }
 
@@ -140,9 +99,14 @@ static int
 ndnfs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
 {
     cout << "ndnfs_read: called with path " << path << endl;
-    
-    if (strcmp(path, file_path) != 0)
+    cout << "ndnfs_read: start read at offset " << offset << " with size " << size << endl;
+
+    auto_ptr<DBClientCursor> cursor = c.query(db_name, QUERY("_id" << path));
+    if (!cursor->more())
         return -ENOENT;
+    
+    BSONObj entry = cursor->next();
+    int file_size = entry.getIntField("size");
 
     if ((size_t)offset >= file_size) /* Trying to read past the end of file. */
         return 0;
@@ -150,18 +114,70 @@ ndnfs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_f
     if (offset + size > file_size) /* Trim the read to the file size. */
         size = file_size - offset;
 
-    memcpy(buf, file_content + offset, size); /* Provide the content. */
+    string content = entry.getStringField("data");
+    memcpy(buf, content.substr(offset, size).c_str(), size); /* Provide the content. */
 
     return size;
 }
 
+static int
+ndnfs_create(const char *path, mode_t mode, struct fuse_file_info *fi)
+{
+    cout << "ndnfs_create: called with path " << path << endl;
+    
+    string abs_path(path);
+    size_t last_comp_pos = abs_path.rfind('/');
+    string dir_path = abs_path.substr(0, last_comp_pos + 1);
+    string file_name = abs_path.substr(last_comp_pos + 1);
+    cout << dir_path << " " << file_name << endl;
+    
+    // Cannot create file that has conflicting file name
+    auto_ptr<DBClientCursor> cursor = c.query(db_name, QUERY("_id" << path));
+    if (!cursor->more())
+        return -EEXIST;
+    
+    // Cannot create file without creating necessary folders
+    cursor = c.query(db_name, QUERY("_id" << dir_path));
+    if (!cursor->more())
+        return -ENOENT;
+    
+    BSONObj entry = cursor->next();
+    
+    // Add new file entry with empty content
+    BSONObj file_entry = BSONObjBuilder().append("_id", path).append("type", file_type).append("mode", mode)
+                            .append("data", "").append("size", 0).obj();
+    c.insert(db_name, file_entry);
+    // Append to existing BSON array
+    c.update(db_name, BSON("_id" << dir_path), BSON( "$push" << BSON( "data" << file_name ) ));
+    
+    return 0;
+}
+
+static int
+ndnfs_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{
+    cout << "ndnfs_write: called with path " << path << endl;
+    cout << "ndnfs_write: start write at offset " << offset << " with size " << size << endl;
+    /*
+    auto_ptr<DBClientCursor> cursor = c.query(db_name, QUERY("_id" << "/"));
+    if (!cursor->more()) {
+        string file_name(path);
+        BSONObj file_entry = BSONObjBuilder().append("name", file_name.substr(1).c_str()).append("type", "file").append("data", buf, size).obj();
+        BSONObj dir_entry = BSON( "name" << "/" << "type" << "dir" << "data" << file_entry);
+        c.insert(db_name, dir_entry);
+    }
+    */
+    return 0;
+}
+
+
 static void
 create_fuse_operations(struct fuse_operations *fuse_op)
 {
-    fuse_op->getattr = ndnfs_getattr; /* To provide size, permissions, etc. */
-    fuse_op->open    = ndnfs_open;    /* To enforce read-only access.       */
-    fuse_op->read    = ndnfs_read;    /* To provide file content.           */
-    fuse_op->readdir = ndnfs_readdir; /* To provide directory listing.      */
+    fuse_op->getattr = ndnfs_getattr;
+    fuse_op->open    = ndnfs_open;
+    fuse_op->read    = ndnfs_read;
+    fuse_op->readdir = ndnfs_readdir;
     //fuse_op->create  = ndnfs_create;
     //fuse_op->write   = ndnfs_write;
 }
@@ -187,11 +203,17 @@ main(int argc, char **argv)
                             .append("data", BSONArrayBuilder().arr()).obj();
         c.insert(db_name, root_dir);
         
-        // Add an initial file for test
+        /* For test use, should remove later */
+        // Add a file
         BSONObj hello_file = BSONObjBuilder().append("_id", "/hello.txt").append("type", file_type).append("mode", 0666)
-                            .append("data", "Hello World\n").obj();
+                            .append("data", "Hello World\n").append("size", 12).obj();
         c.insert(db_name, hello_file);
         c.update(db_name, BSON( "_id" << "/" ), BSON( "$push" << BSON( "data" << "hello.txt" ) ) );
+        // Add a folder
+        BSONObj hello_dir = BSONObjBuilder().append("_id", "/hello").append("type", dir_type).append("mode", 0777)
+                            .append("data", BSONArrayBuilder().arr()).obj();
+        c.insert(db_name, hello_dir);
+        c.update(db_name, BSON( "_id" << "/" ), BSON( "$push" << BSON( "data" << "hello" ) ) );
     }
     cout << "Root directory mounted from database" << endl;
     
