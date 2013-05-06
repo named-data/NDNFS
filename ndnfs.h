980ebf340cebf176b417a922656a340409326405
Wentao Shang <wentaoshang@gmail.com>
2013-05-06 13:15:03 -0700

    Add copyright note

--- ndnfs.h
+++ ndnfs.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2013 University of California, Los Angeles
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Wentao Shang <wentao@cs.ucla.edu>
+ */
+
 #ifndef NDNFS_H
 #define NDNFS_H
 
@@ -23,21 +42,6 @@ extern const int version_type;
 extern const int segment_type;
 
 
-inline int split_last_component(const char *path, std::string &prefix, std::string &name)
-{
-    std::string abs_path(path);
-    size_t last_comp_pos = abs_path.rfind('/');
-    if (last_comp_pos == std::string::npos)
-        return -1;
-    
-    prefix = abs_path.substr(0, last_comp_pos);
-    if (prefix.empty())
-        prefix = "/";
-    name = abs_path.substr(last_comp_pos + 1);
-    
-    return 0;
-}
-
 inline int split_last_component(const std::string& path, std::string &prefix, std::string &name)
 {
     size_t last_comp_pos = path.rfind('/');
@@ -50,6 +54,12 @@ inline int split_last_component(const std::string& path, std::string &prefix, st
     name = path.substr(last_comp_pos + 1);
     
     return 0;
+}
+
+inline int split_last_component(const char *path, std::string &prefix, std::string &name)
+{
+    std::string path_string(path);
+    return split_last_component(path_string, prefix, name);
 }
 
 #endif
