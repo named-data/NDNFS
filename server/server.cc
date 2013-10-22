97090880a2330b171426766517cb3586a7e5f4a3
Jeff Thompson <jefft0@gmail.com>
2013-10-21 17:42:32 -0700

    Server.cc doesn't need to push certs.

--- server/server.cc
+++ server/server.cc
@@ -51,28 +51,6 @@ ptr_lib::shared_ptr<Face> handler(new Face(ndnTransport, ptr_lib::make_shared<Tc
 
 string global_prefix;
 
-void
-publishAllCert()
-{
-    cerr << "push all cert!" << endl;
-    sqlite3 * fakeDB;
-    int res = sqlite3_open("/Users/ndn/qiuhan/NDNFS/fake-data.db", &fakeDB);
-  
-    if (res != SQLITE_OK)
-        cerr << "damn it!" << endl;
-
-    sqlite3_stmt *stmt;
-    sqlite3_prepare_v2 (fakeDB, "SELECT data_blob FROM data", -1, &stmt, 0);
-
-    while(sqlite3_step(stmt) == SQLITE_ROW)
-    {
-        cerr << "publish certificate" << endl;
-        ndnTransport->send((const uint8_t*)sqlite3_column_blob(stmt, 0), sqlite3_column_bytes(stmt, 0));
-    }
-
-    sqlite3_close (fakeDB);
-}
-
 int main(int argc, char **argv) {
     const char* prefix = "/ndn/ucla.edu/qiuhan/dummy/ndnfs";
     int opt;
@@ -89,18 +67,7 @@ int main(int argc, char **argv) {
             break;
         }
     }
-    
-    publishAllCert();
-    Name certificatedsk_name("/ndn/ucla.edu/qiuhan/DSK-1378423613/ID-CERT/1378423803");
-    SignedBlob cert0 = keychain->getAnyCertificate(certificatedsk_name)->wireEncode();
-    cerr << "publish DSK: " << cert0->size() << endl;
-    ndnTransport->send(*cert0);
-
-    Name certificateksk_name("/ndn/ucla.edu/qiuhan/KSK-1378422677/ID-CERT/1378423300");
-    SignedBlob cert1 = keychain->getAnyCertificate(certificateksk_name)->wireEncode();
-    cerr << "publish KSK: " << cert1->size() << endl;
-    ndnTransport->send(*cert1);
-    
+        
     cout << "main: open sqlite database" << endl;
 
     if (sqlite3_open(db_name, &db) == SQLITE_OK) {
@@ -118,7 +85,8 @@ int main(int argc, char **argv) {
 
     handler->registerPrefix(InterestBaseName, ::OnInterest, ::OnRegisterFailed);
     while (true) {
-        sleep (1);
+        handler->processEvents();
+        usleep (10000);
     }
 	
     cout << "main(): ServerModule exiting ..." << endl;
